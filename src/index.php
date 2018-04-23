9593523a4c8b3532658a1b3e328632ce626f9b0a
kseta <keigo.seta@gmail.com>
2018-04-23 14:54:34 +0900

    rm php scripts

--- src/index.php
+++ src/index.php
@@ -20,9 +20,6 @@
                 <p>TRY EVERYTHING!</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
-                        echo fread($myfile,filesize("/var/www/my-vol/date"));
-                        fclose($myfile);
                 ?>
 
             </div>
